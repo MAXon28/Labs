4fdfa447b0a4481a0f45240fd022b97014c91cf9
Max <max.ronald9@gmail.com>
2019-03-03 17:34:17 +0300

    second

--- First lab/Lab1.1_by_28MAXon/Lab1.1_by_28MAXon/Complex.h
+++ First lab/Lab1.1_by_28MAXon/Lab1.1_by_28MAXon/Complex.h
@@ -1,8 +1,8 @@
 #ifndef Complex_h
 #define Complex_h
 struct komplex {
-	double x; // Действительная часть
-	double y; // Мнимая часть
+	double x; // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+	double y; // пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
 };
 void sum(komplex komplex_number_1, komplex komplex_number_2);
 void sub(komplex komplex_number_1, komplex komplex_number_2);
--- First lab/Lab1.1_by_28MAXon/Lab1.1_by_28MAXon/Lab1.1_by_28MAXon.cpp
+++ First lab/Lab1.1_by_28MAXon/Lab1.1_by_28MAXon/Lab1.1_by_28MAXon.cpp
@@ -1,24 +1,68 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <iostream>
+#include <string>
 #include <locale.h>
 #include "Complex.h"
 using namespace std;
+class complex
+{
+public:
+	int control()
+	{
+		double t;
+		while (!(cin >> t) || cin.get() != '\n')
+		{
+			cout << " Ошибка ввода! Повторите ввод заново:\n";
+			cin.clear();
+			cin.sync();
+		}
+		return t;
+	}
+	bool verification(int o)
+	{
+		if (o == 0 || o > 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+};
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	komplex komplex_number_one;
 	komplex komplex_number_two;
 	char oper;
+	string oper;
 	cout << " Общий вид комплексного числа:\n z = x + y * i\n";
 	cout << " Введите первое комплексное число (сначала действительную часть, а затем мнимую):\n";
 	cin >> komplex_number_one.x >> komplex_number_one.y;
 	cout << " Введите второе комплексное число (сначала действительную часть, а затем мнимую):\n";
 	cin >> komplex_number_two.x >> komplex_number_two.y;
+	cout << " Введите второе комплексное число\n Cначала действительную часть:\n";
+	komplex_number_two.x = check.control();
+	cout << " Теперь мнимую часть:\n";
+	komplex_number_two.y = check.control();
 	cout << " Вид первого комплексного числа: z1 = " << komplex_number_one.x << " + ( " << komplex_number_one.y << ") * i\n";
 	cout << " Вид второго комплексного числа: z2 = " << komplex_number_two.x << " + ( " << komplex_number_two.y << ") * i\n";
 	cout << "\t\t\t\t\t\tКАЛЬКУЛЯТОР\n Выберите действие из нижеприведённого списка. Нажмите знак, который соответствует выбранному действию.\n + сложение\n - вычитание\n * умножение\n / деление\n";
+op:
 	cin >> oper;
 	switch (oper)
+	{
+		cout << "Ошибка ввода!\n Повторите ввод. Выберите действие из нижеприведённого списка. Нажмите знак, который соответствует выбранному действию.\n + сложение\n - вычитание\n * умножение\n / деление\n";
+		goto op;
+	}
+	char operation = oper[0];
+	if (operation != '+' || operation != '-' || operation != '*' || operation != '/')
+	{
+		cout << "Ошибка ввода!\n Повторите ввод. Выберите действие из нижеприведённого списка. Нажмите знак, который соответствует выбранному действию.\n + сложение\n - вычитание\n * умножение\n / деление\n";
+		goto op;
+	}
+	switch (operation)
 	{
 	case '+':
 		sum(komplex_number_one, komplex_number_two);
